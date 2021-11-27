3f5b0defd6debd957c5750695871c6a22c785e32
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-11-27 21:02:18 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+bold=$(echo -en "\e[1m")
+nc=$(echo -en "\e[0m")
+lightblue=$(echo -en "\e[94m")
+lightgreen=$(echo -en "\e[92m")
+clear
+
+echo "
+${bold}${lightgreen}========================================================================                            
+${bold}${lightgreen}  Lavalink Server                                                                                                 
+${bold}${lightgreen}========================================================================
+ "
+ 
+echo "${nc}"
+
+if [[ -f "./installed" ]]; 
+
+then
+    echo "Starting the server"
+    sleep 1
+    java -jar Lavalink.jar
+
+else
+
+    echo "Downloading files for Lavalink Server"
+    sleep 1
+    touch installed
+    wget https://github.com/Cog-Creators/Lavalink-Jars/releases/download/3.3.2.3_1239/Lavalink.jar
+    wget application.yml https://raw.githubusercontent.com/afnan007a/Lavalink-MusicBot/main/application.yml
+    java -jar Lavalink.jar
+
+fi
